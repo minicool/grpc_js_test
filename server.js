118de83267271019c6a2ec313b35b395bb16b083
DawdlerMan <minicool.software@gamil.com>
2017-05-08 17:40:01 +0800

    modify by test

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var services = require('./DeviceServer_grpc_pb.js');  
 var messages = require('./DeviceServer_pb.js');  
 var grpc = require('grpc')  
-var hello = function(call, callback) {  
-//  var response = new messages.;  
-//  response.setHellostring("hello," + call.request.getName());  
-   new messages.
+
+var RFIDcard_writeData = function(call, callback) {  
+  var response = new messages.RFIDcard_writeData_reply;  
+  response.setAssetsGloballd("1234567890123");
+  response.setSuccess(true);
   callback(null, response);  
 }  
 var server = new grpc.Server();  
 server.addService(  
   services.RFIDDeviceServerService,
   {  
-    hello:hello  
+    RFIDcard_writeData:RFIDcard_writeData  
   }  
 );  
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());  
+server.bind('0.0.0.0:40051', grpc.ServerCredentials.createInsecure());  
 server.start(function(err,data){  
   console.log(err);  
   console.log(data);  
